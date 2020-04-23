8eca645d6b382cb3dbd448c6e33f9f28c0b08938
Andy J Rivera <rivera.andy40@gmail.com>
2020-04-23 14:03:22 -0400

    Update LoginServlet.java

--- src/LoginPackage/LoginServlet.java
+++ src/LoginPackage/LoginServlet.java
@@ -33,5 +33,37 @@ public class LoginServlet extends HttpServlet {
 			System.out.println(theException);
 		}
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String email = request.getParameter("email");
+		
+		if(username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+			RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+			//out.println("<font color=red>Please fill all the fields</font>");
+			rd.include(request, response);
+		}else {
+			try {
+				Account acc = new Account();
+				acc.setUsername(username);
+				acc.setPassword(password);
+				acc.setEmail(email);
+				
+				int status = AccountDAO.register(acc);
+				if(status == 1) {
+					response.sendRedirect("registerSuccess.jsp");	//logged-in page
+				}else if(status == 2){
+					response.sendRedirect("registerUsernameExists.jsp");	//this needs to be changed, 
+				}else if(status == 3){
+					response.sendRedirect("registerEmailExists.jsp");
+				}else {
+					response.sendRedirect("registerExists.jsp");
+				}
+			}catch(Throwable theException) {
+				System.out.println(theException);
+			}
+		}
+	}
 
 }
